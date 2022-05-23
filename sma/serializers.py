6113a076b3513eb5eb764ed0f078318911406281
JuniorTenorio16 <juniortenorio1016@gmail.com>
2022-05-23 05:59:29 -0300

    create serializer CandlesSerializer

--- sma/serializers.py
+++ sma/serializers.py
@@ -1,9 +1,20 @@
 from dataclasses import fields
 from pyexpat import model
+from sqlite3 import Timestamp
 from rest_framework import serializers
 from sma.models import Sma
 
+
 class SmaSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Sma
-        fields=('pair', 'timestamp', 'sma_20', 'sma_50', 'sma_200')
+        model = Sma
+        fields = ('pair', 'timestamp', 'sma_20', 'sma_50', 'sma_200')
+
+
+class CandlesSerializer(serializers.Serializer):
+    close = serializers.DecimalField(max_digits=19, decimal_places=10)
+    high = serializers.DecimalField(max_digits=19, decimal_places=10)
+    low = serializers.DecimalField(max_digits=19, decimal_places=10)
+    open = serializers.DecimalField(max_digits=19, decimal_places=10)
+    timestamp = serializers.IntegerField()
+    volume = serializers.DecimalField(max_digits=19, decimal_places=10)
