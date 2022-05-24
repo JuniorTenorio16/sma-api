3aa57303edf78cce5baa07ab2ac7b5648fca44a3
JuniorTenorio16 <juniortenorio1016@gmail.com>
2022-05-24 06:35:24 -0300

    create method validateDateFromTo

--- sma/views.py
+++ sma/views.py
@@ -1,22 +1,45 @@
+from django.forms import ValidationError
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
+from datetime import datetime
 
 from sma.models import Sma
 from sma.serializers import SmaSerializer
 # Create your views here.
 
+
+def validateDateFromTo(from_date, to_date):
+    from_date = datetime.fromtimestamp(from_date)
+    to_date = datetime.fromtimestamp(to_date)
+    diff_date = (to_date-from_date).days
+
+    if (datetime.now().date()-to_date.date()).days < 1:
+        return False
+    
+    if diff_date < 1 and diff_date > 365:
+        return False
+    
+    return True
+
+
 @csrf_exempt
-def smaApi(request):
+def smaApi(request, pair):
     if request.method=='GET':
-        sma = Sma.objects.all()
-        sma_serializer = SmaSerializer(sma, many=True)
-        return JsonResponse(sma_serializer.data, safe=False)
-    if request.method=='POST':
-        sma_data = JSONParser().parse(request)
-        sma_serializer = SmaSerializer(data=sma_data)
-        if sma_serializer.is_valid():
-            sma_serializer.save()
-            return JsonResponse('Added Successfully', safe=False)
-        return JsonResponse('Failed to Add', safe=False)
+        print(pair)
+        sma_body = JSONParser().parse(request)
+        if validateDateFromTo(sma_body.get('from'), sma_body.get('to')):
+            try:
+                sma = Sma.objects.all()
+                sma_serializer = SmaSerializer(sma, many=True)
+            except ValidationError as err:
+                raise serializers.ValidationError('Error request mongo: %s'%err)
+        else:
+            JsonResponse({
+                    'status': 'false', 
+                    'message': 'Date format or values ​​is invalid'
+                }, 
+                status=422)
+    return JsonResponse(sma_body, safe=False)
+    
