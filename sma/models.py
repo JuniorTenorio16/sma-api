33d375b75591e98937283bd84379923cb580eaf4
JuniorTenorio16 <juniortenorio1016@gmail.com>
2022-05-24 03:10:26 -0300

    update migrate  Sma

--- sma/models.py
+++ sma/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class Sma(models.Model):
     pair = models.CharField(max_length=6, blank=False, default='BRLBTC')
-    timestamp = models.DateField()
-    close = models.DecimalField(max_digits=19, decimal_places=10)
-    sma_20 = models.DecimalField(max_digits=19, decimal_places=10)
-    sma_50 = models.DecimalField(max_digits=19, decimal_places=10)
-    sma_200 = models.DecimalField(max_digits=19, decimal_places=10)
+    timestamp = models.IntegerField()
+    close = models.FloatField(blank=True)
+    sma_20 = models.FloatField(null=True)
+    sma_50 = models.FloatField(null=True)
+    sma_200 = models.FloatField(null=True)
