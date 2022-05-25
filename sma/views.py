1242776697388148a8ccedce1ca973ba646cd02f
JuniorTenorio16 <juniortenorio1016@gmail.com>
2022-05-25 02:41:28 -0300

    create method validateRange

--- sma/views.py
+++ sma/views.py
@@ -1,3 +1,4 @@
+from sqlite3 import Timestamp
 from django.forms import ValidationError
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
@@ -24,22 +25,36 @@ def validateDateFromTo(from_date, to_date):
     return True
 
 
+def validateRange(range):
+    range_valid = [20, 50, 200]
+    if range not in range_valid:
+        return False
+    return True
+
+
+
 @csrf_exempt
 def smaApi(request, pair):
     if request.method=='GET':
         print(pair)
         sma_body = JSONParser().parse(request)
-        if validateDateFromTo(sma_body.get('from'), sma_body.get('to')):
+        if not validateDateFromTo(sma_body.get('from'), sma_body.get('to')):
+            JsonResponse({
+                    'status': 'false', 
+                    'message': 'Date format or values ​​is invalid'
+                }, 
+                status=422)
+        if validateRange(sma_body.get('range')):
             try:
-                sma = Sma.objects.all()
+                sma = Sma.objects.filter(timestamp__range=(sma_body.get('from'), sma_body.get('to')))
                 sma_serializer = SmaSerializer(sma, many=True)
             except ValidationError as err:
                 raise serializers.ValidationError('Error request mongo: %s'%err)
         else:
             JsonResponse({
                     'status': 'false', 
-                    'message': 'Date format or values ​​is invalid'
+                    'message': 'Range value is invalid'
                 }, 
                 status=422)
-    return JsonResponse(sma_body, safe=False)
+    return JsonResponse(sma_serializer, safe=False)
     
