f086c19505cec401c57ecfc4ecb88026d7fd3124
JuniorTenorio16 <juniortenorio1016@gmail.com>
2022-05-25 15:18:17 -0300

    create model RecordMissing

--- sma/models.py
+++ sma/models.py
@@ -9,3 +9,9 @@ class Sma(models.Model):
     sma_20 = models.FloatField(null=True)
     sma_50 = models.FloatField(null=True)
     sma_200 = models.FloatField(null=True)
+
+
+class RecordMissing(models.Model):
+    timestamp = models.IntegerField()
+    pair = models.CharField(max_length=6, blank=False, default='BRLBTC')
+    status = models.BooleanField(default=True)
