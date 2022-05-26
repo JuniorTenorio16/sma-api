d344dfe581526d032b69053c38d43d523bf794da
JuniorTenorio16 <juniortenorio1016@gmail.com>
2022-05-26 05:53:40 -0300

    use utils.py in views.py

--- sma/views.py
+++ sma/views.py
@@ -2,52 +2,11 @@ from django.forms import ValidationError
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
-from datetime import datetime
-import json
 
 from sma.models import Sma
 from sma.serializers import SmaSerializer
-
-
-def validateDateFromTo(from_date, to_date):
-    from_date = datetime.fromtimestamp(from_date)
-    to_date = datetime.fromtimestamp(to_date)
-    diff_date = (to_date-from_date).days
-
-    if (datetime.now().date()-to_date.date()).days < 1:
-        return False
-
-    if diff_date < 1 or diff_date > 365:
-        return False
-    
-    return True
-
-
-def validateRange(range):
-    range_valid = [20, 50, 200]
-    if range not in range_valid:
-        return False
-    return True
-
-
-def buildBodyResponse(data, range):
-    body = []
-    for sma in data:
-        body.append({
-            'timestamp': sma.get('timestamp'),
-            'mms': sma.get('sma_%s'%range)
-        })
-    return body
-
-
-def validateJsonRequest(data):
-    if "to" not in data:
-        return False
-    if "from" not in data:
-        return False
-    if "range" not in data:
-        return False
-    return True
+from sma.utils import buildBodyResponse,\
+     validateDateFromTo, validateJsonRequest, validateRange 
 
 
 @csrf_exempt
@@ -58,7 +17,7 @@ def smaApi(request, pair):
         return JsonResponse({
             'status': 'false', 
             'message': 'Json is invalid'
-        })
+        },status=422)
     if validateJsonRequest(sma_body) is False:
         return JsonResponse({
                     'status': 'false', 
